ccdb2da475dde327a59e1b481566ab05f3f033c5
Raymond713 <160208894+Raymond713@users.noreply.github.com>
2025-07-09 14:13:20 +0800

    app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def data():
     return jsonify({"message": "Hello from Railway!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # <-- 預設用 5000，但線上用 Railway 分配的
+    app.run(host='0.0.0.0', port=port)
