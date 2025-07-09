89779f861e7d779addbc6ecf04a41bed6c6f4ddd
Raymond713 <160208894+Raymond713@users.noreply.github.com>
2025-07-09 15:47:38 +0800

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template_string
 import threading
 import time
 import os
 
 app = Flask(__name__)
 
-# 初始 JSON 結構
+# Global parking data
 parking_data = {
     "on_street": [],
     "off_street": [
@@ -32,21 +32,33 @@ parking_data = {
                 "type": "Polygon",
                 "coordinates": [
                     [
-                        [-87.615809767461, 41.8392281200473],
-                        [-87.6158117863599, 41.8394410905166],
-                        [-87.6149627294214, 41.8394511419997],
-                        [-87.6149609350484, 41.8393400416038],
-                        [-87.6154167060568, 41.8393336222849],
-                        [-87.6154149022188, 41.8392329423387],
-                        [-87.615809767461, 41.8392281200473]
+                        [-87.61580976746095, 41.83922812004734],
+                        [-87.6158117863599, 41.83944109051658],
+                        [-87.61496272942144, 41.839451141999724],
+                        [-87.61496093504843, 41.83934004160383],
+                        [-87.61541670605679, 41.83933362228489],
+                        [-87.61541490221879, 41.839232942338654],
+                        [-87.61580976746095, 41.83922812004734]
                     ]
                 ]
             }
         }
-    ]
+    ],
+    "config": {
+        "config_version": "2025.06.12",
+        "update_required": True,
+        "apply_on_next_boot": False,
+        "parameters": {
+            "api_key": "https://guidance.streetline.com/v3/guidance/by-customer?customer=api_demo_1&api_key=XXXXXXXXXXXXXXXXXX",
+            "uid": 0,
+            "interval": 15,
+            "front_light_level": 10
+        }
+    }
 }
 
-# 背景執行：每 2 分鐘變動 estimated_spaces
+# Background thread to simulate changing estimated_spaces
+
 def update_spaces():
     value = 0
     direction = 1
@@ -59,15 +71,47 @@ def update_spaces():
         elif value == 0:
             direction = 1
 
-# 啟動背景執行緒
 threading.Thread(target=update_spaces, daemon=True).start()
 
-# API 路由
+# API endpoint to get data
 @app.route('/api/data')
-def get_parking_data():
+def get_data():
     return jsonify(parking_data)
 
-# 啟動伺服器（本地或 Railway 用）
+# API endpoint to update config parameters
+@app.route('/api/update_config', methods=['POST'])
+def update_config():
+    data = request.json
+    for key in ["api_key", "uid", "interval", "front_light_level"]:
+        if key in data:
+            parking_data["config"]["parameters"][key] = data[key]
+    return jsonify({"status": "updated", "config": parking_data["config"]})
+
+# Frontend form to edit config
+@app.route('/')
+def index():
+    form_html = """
+    <html><body>
+    <h2>Update Parking Config</h2>
+    <form action="/update" method="post">
+        API Key:<br><input type="text" name="api_key" value="{{config['parameters']['api_key']}}"><br>
+        UID:<br><input type="number" name="uid" value="{{config['parameters']['uid']}}"><br>
+        Interval:<br><input type="number" name="interval" value="{{config['parameters']['interval']}}"><br>
+        Front Light Level:<br><input type="number" name="front_light_level" value="{{config['parameters']['front_light_level']}}"><br><br>
+        <input type="submit" value="Update">
+    </form>
+    </body></html>
+    """
+    return render_template_string(form_html, config=parking_data["config"])
+
+@app.route('/update', methods=['POST'])
+def update():
+    for key in ["api_key", "uid", "interval", "front_light_level"]:
+        if key in request.form:
+            val = request.form[key]
+            parking_data["config"]["parameters"][key] = int(val) if key in ["uid", "interval", "front_light_level"] else val
+    return "<p>Config Updated</p><a href='/'>Back</a>"
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
