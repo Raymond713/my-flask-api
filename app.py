b53a59a833eeeb615d096ac00d30c40b1bebe3c6
Raymond713 <160208894+Raymond713@users.noreply.github.com>
2025-07-09 11:57:57 +0800

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/data')
 def data():
-    return jsonify({"message": "Hello from Render!"})
+    return jsonify({"message": "Hello from Railway!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=5000)
